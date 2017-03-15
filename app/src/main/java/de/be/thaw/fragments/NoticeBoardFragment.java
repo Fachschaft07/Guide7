d36d92f0b1f4deca9083680b1163fd4cc4c1738c
BullshitPingu <barny.eder@gmail.com>
2017-03-15 19:10:51 +0100

    Links are now clickable in the notice board.

--- app/src/main/java/de/be/thaw/fragments/NoticeBoardFragment.java
+++ app/src/main/java/de/be/thaw/fragments/NoticeBoardFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
