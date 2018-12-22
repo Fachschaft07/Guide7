8461b45c8da460d3df43389f292782d213da7ae7
Benjamin Eder <barny.eder@gmail.com>
2018-12-22 21:21:55 +0100

    Notice board entry style update

--- lib/ui/view/notice_board/entry/notice_board_entry_widget.dart
+++ lib/ui/view/notice_board/entry/notice_board_entry_widget.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:guide7/model/notice_board/notice_board_entry.dart';
 import 'package:flutter_html_view/flutter_html_view.dart';
+import 'package:guide7/util/custom_colors.dart';
+import 'package:intl/intl.dart';
 
 /// Widget displaying a notice board entry.
 class NoticeBoardEntryWidget extends StatelessWidget {
+  /// Date format to format the notice board entry dates.
+  static DateFormat _dateFormat = DateFormat("dd.MM.yy");
+
   /// Notice board entry to show.
   final NoticeBoardEntry entry;
 
@@ -22,22 +27,44 @@ class NoticeBoardEntryWidget extends StatelessWidget {
           top: 20.0,
           bottom: 20.0,
         ),
-        decoration: isLast ? null : BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12))), // Divider between entries
+        decoration: isLast
+            ? null
+            : BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Theme.of(context).dividerColor,
+                  ),
+                ),
+              ), // Divider between entries
         child: Column(
           children: <Widget>[
             Text(
               entry.title,
               style: TextStyle(fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
-            ),
-            Text(
-              entry.author,
-              style: TextStyle(fontStyle: FontStyle.italic),
+              textScaleFactor: 1.1,
             ),
             HtmlView(
               data: entry.content,
-              padding: EdgeInsets.all(0),
-            )
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.person,
+                  color: CustomColors.slateGrey,
+                ),
+                Expanded(
+                  child: Text(
+                    entry.author,
+                  ),
+                ),
+                Icon(Icons.timelapse, color: CustomColors.slateGrey),
+                Expanded(
+                  child: Text("${_dateFormat.format(entry.validFrom)} bis ${_dateFormat.format(entry.validTo)}"),
+                )
+              ],
+            ),
           ],
         ),
       );
